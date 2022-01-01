0c0dde65ba1526f46abc50cfcdd82740426f59bb
Hamza Halaweh <78810452+hhalaweh@users.noreply.github.com>
2022-01-01 16:52:42 +0200

    Fixing headers and adding tweet preview

--- App.py
+++ App.py
@@ -5,9 +5,26 @@ import config
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 import streamlit as st
+import requests
+import streamlit.components.v1 as components
 
-st.title("Stock Prediction using Twitter Sentiment Analysis Web App")
-st.subheader("""Twitter sentiment analysis web application developed for **Software Engineering** project 2021-2022""")
+
+class get_html_tweet(object):
+    def __init__(self, s, embed_str=False):
+        if not embed_str:
+            # Use Twitter's oEmbed API
+            # https://dev.twitter.com/web/embedded-tweets
+            api = "https://publish.twitter.com/oembed?url={}".format(s)
+            response = requests.get(api)
+            self.text = response.json()["html"]
+        else:
+            self.text = s
+
+    def _repr_html_(self):
+        return self.text
+
+    def component(self):
+        return components.html(self.text, height=600)
 
 def authenticate():
   client  = tweepy.Client(bearer_token = config.BEARER_TOKEN,
@@ -17,9 +34,6 @@ def authenticate():
                           access_token_secret = config.ACCESS_TOKEN_SECRET)
   return client
 
-def percentage(part, whole):
-  return 100 * float(part)/float(whole)
-
 def get_tweets(query, number_of_tweets):
   global tweets_list 
   tweets_list = []
@@ -27,10 +41,13 @@ def get_tweets(query, number_of_tweets):
   client = authenticate()
   tweets = client.search_recent_tweets(query = query, max_results = 100)
   tweet_data = tweets.data
+  global tweet_link_example
+  tweet_link_example = '' 
   
 
   for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
                               tweet_fields=['context_annotations', 'created_at'], max_results=100).flatten(limit=number_of_tweets):
+    x = tweet
     analysis = TextBlob(tweet.text)
     polarity += analysis.sentiment.polarity
 
@@ -42,14 +59,18 @@ def get_tweets(query, number_of_tweets):
       positive += 1
     tweets_list.append(tweet.text)
 
-  #positive = format(percentage(positive, number_of_tweets), '.2f')
-  #negative = format(percentage(negative, number_of_tweets), '.2f')
-  #neutral = format(percentage(neutral, number_of_tweets), '.2f')
+  tweet_link_example = f"https://twitter.com/user/status/{tweet.id}"
   
   return positive, negative, neutral
-st.subheader("Enter the name of the stock/financial asset you would like to check the sentiment of in real time.")
+
+
+st.title("Stock Prediction using Twitter Sentiment Analysis Web App")
+st.subheader("""Twitter sentiment analysis web application developed for **Software Engineering** project 2021-2022""")
+st.markdown('##')
+
+st.markdown("###Enter the name of the stock/financial asset you would like to check the sentiment of in real time.")
 keyword = st.text_input("Keyword", 'Bitcoin')
-st.subheader("Enter the number of tweets the Twitter API should extract concerning the specific keyword.")
+st.markdown("###Enter the number of tweets the Twitter API should extract concerning the specific keyword.")
 number_of_tweets = st.slider('Number of Tweets', min_value=100, max_value=500, value=100, step=30)
 result = st.button("Analyze")
 
@@ -63,3 +84,5 @@ if(result):
           shadow=True, startangle=90)
   ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
   st.pyplot(fig1)
+  st.subheader("Extracted Tweet Example")
+  t = Tweet(tweet_link_example).component()
