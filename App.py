cab8c4fa987e8a0c1a8ee8907667e31d782f857d
Hamza Halaweh <78810452+hhalaweh@users.noreply.github.com>
2022-01-01 17:29:59 +0200

    Changing colors of pie chart

--- App.py
+++ App.py
@@ -81,7 +81,7 @@ if(result):
   sizes = [pos, neu, neg]
   fig1, ax1 = plt.subplots()
   ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-          shadow=True, startangle=90, colors = ['green', 'red', 'orange'])
+          shadow=True, startangle=90, colors = ['#31333F', '#F0F2F6', '#FF4B4B'])
   ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
   st.pyplot(fig1)
   st.subheader("Extracted Tweet Example")
